0b12a3e1720d652412e105428188458847838924
Astronaut <chen.chang0123@gmail.com>
2022-11-29 18:08:36 +0800

    64 v2

--- src/main/java/leetcode/editor/en/MinimumPathSum.java
+++ src/main/java/leetcode/editor/en/MinimumPathSum.java
@@ -1,7 +1,5 @@
 package leetcode.editor.en;
 
-import java.util.Arrays;
-
 /**
  * Given a m x n grid filled with non-negative numbers, find a path from top left
  * to bottom right, which minimizes the sum of all numbers along its path.
@@ -43,31 +41,24 @@ public class MinimumPathSum {
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
-        int[][] memo;
-
         public int minPathSum(int[][] grid) {
             int m = grid.length;
             int n = grid[0].length;
-            memo = new int[m][n];
-            for (int[] row : memo) {
-                Arrays.fill(row, -1);
-            }
-            return dp(grid, m - 1, n - 1);
-        }
+            int[][] dp = new int[m][n];
 
-        public int dp(int[][] grid, int i, int j) {
-            if (i == 0 && j == 0) {
-                return grid[0][0];
+            dp[0][0] = grid[0][0];
+            for (int i = 1; i < m; i++) {
+                dp[i][0] = dp[i - 1][0] + grid[i][0];
             }
-            if (i < 0 || j < 0) {
-                return Integer.MAX_VALUE;
+            for (int j = 1; j < n; j++) {
+                dp[0][j] = dp[0][j - 1] + grid[0][j];
             }
-            if (memo[i][j] != -1) {
-                return memo[i][j];
+            for (int i = 1; i < m; i++) {
+                for (int j = 1; j < n; j++) {
+                    dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+                }
             }
-            memo[i][j] = Math.min(dp(grid, i - 1, j), dp(grid, i, j - 1)) + grid[i][j];
-
-            return memo[i][j];
+            return dp[m - 1][n - 1];
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
