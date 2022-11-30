281ac9541b32ca8ed8d01910f8d0fd45b9f2d438
Astronaut <chen.chang0123@gmail.com>
2022-11-30 16:20:57 +0800

    200v1

---
+++ src/main/java/leetcode/editor/en/NumberOfIslands.java
@@ -0,0 +1,87 @@
+package leetcode.editor.en;
+
+/**
+ * Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0
+ * 's (water), return the number of islands.
+ * <p>
+ * An island is surrounded by water and is formed by connecting adjacent lands
+ * horizontally or vertically. You may assume all four edges of the grid are all
+ * surrounded by water.
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * <p>
+ * Input: grid = [
+ * ["1","1","1","1","0"],
+ * ["1","1","0","1","0"],
+ * ["1","1","0","0","0"],
+ * ["0","0","0","0","0"]
+ * ]
+ * Output: 1
+ * <p>
+ * <p>
+ * Example 2:
+ * <p>
+ * <p>
+ * Input: grid = [
+ * ["1","1","0","0","0"],
+ * ["1","1","0","0","0"],
+ * ["0","0","1","0","0"],
+ * ["0","0","0","1","1"]
+ * ]
+ * Output: 3
+ * <p>
+ * <p>
+ * <p>
+ * Constraints:
+ * <p>
+ * <p>
+ * m == grid.length
+ * n == grid[i].length
+ * 1 <= m, n <= 300
+ * grid[i][j] is '0' or '1'.
+ * <p>
+ * <p>
+ * Related Topics Array Depth-First Search Breadth-First Search Union Find Matrix
+ * 👍 18028 👎 410
+ */
+public class NumberOfIslands {
+    public static void main(String[] args) {
+        Solution solution = new NumberOfIslands().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int numIslands(char[][] grid) {
+            int res = 0;
+            int m = grid.length, n = grid[0].length;
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (grid[i][j] == '1') {
+                        res++;
+                        dfs(grid, i, j);
+                    }
+                }
+            }
+            return res;
+        }
+
+        void dfs(char[][] grid, int i, int j) {
+            int m = grid.length, n = grid[0].length;
+            if (i < 0 || j < 0 || i >= m || j >= n) {
+                return;
+            }
+            if (grid[i][j] == '0') {
+                return;
+            }
+            grid[i][j] = '0';
+            dfs(grid, i + 1, j);
+            dfs(grid, i, j + 1);
+            dfs(grid, i - 1, j);
+            dfs(grid, i, j - 1);
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+} 
