6628eadf5706c748cb1061e3c4cb94cdf8e15fbd
Astronaut <chen.chang0123@gmail.com>
2022-12-22 15:12:33 +0800

    714v1

--- src/main/java/leetcode/editor/en/BestTimeToBuyAndSellStockIi.java
+++ src/main/java/leetcode/editor/en/BestTimeToBuyAndSellStockIi.java
@@ -51,25 +51,27 @@ package leetcode.editor.en;
 // Related Topics Array Dynamic Programming Greedy 👍 9909 👎 2482
 
 public class BestTimeToBuyAndSellStockIi {
-    public static void main(String[] args){
-        Solution solution = new BestTimeToBuyAndSellStockIi().new Solution();}
-    //leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
-    public int maxProfit(int[] prices) {
-        int n = prices.length;
-        int[][] dp = new int[n][2];
-        for (int i = 0; i < n; i++) {
-            if (i - 1 == -1) {
-                dp[i][0] = 0;
-                dp[i][1] = -prices[i];
-                continue;
-            }
-            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
-            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0]-prices[i]);
-        }
-        return dp[n - 1][0];
+    public static void main(String[] args) {
+        Solution solution = new BestTimeToBuyAndSellStockIi().new Solution();
     }
-}
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int maxProfit(int[] prices) {
+            int n = prices.length;
+            int[][] dp = new int[n][2];
+            for (int i = 0; i < n; i++) {
+                if (i - 1 == -1) {
+                    dp[i][0] = 0;
+                    dp[i][1] = -prices[i];
+                    continue;
+                }
+                dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+                dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+            }
+            return dp[n - 1][0];
+        }
+    }
 //leetcode submit region end(Prohibit modification and deletion)
 
 } 
