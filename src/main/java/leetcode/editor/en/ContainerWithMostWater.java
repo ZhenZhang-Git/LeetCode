e8080337770d9afb26ef77c9cfaa1d09a6fca0f1
Astronaut <chen.chang0123@gmail.com>
2023-03-15 17:10:36 +0800

    11_m1

--- src/main/java/leetcode/editor/en/ContainerWithMostWater.java
+++ src/main/java/leetcode/editor/en/ContainerWithMostWater.java
@@ -50,16 +50,17 @@ public class ContainerWithMostWater {
     class Solution {
         public int maxArea(int[] height) {
             int i = 0, j = height.length - 1;
-            int maxArea = 0;
+            int area = 0, maxArea = 0;
             while (i < j) {
-                int area = Math.min(height[i], height[j]) * (j - i);
-                if (height[i] > height[j]) {
-                    j--;
-                } else {
+                area = Math.min(height[i], height[j]) * (j - i);
+                maxArea = Math.max(maxArea, area);
+                if (height[i] <= height[j]) {
                     i++;
+                } else {
+                    j--;
                 }
-                maxArea = Math.max(area, maxArea);
             }
+
             return maxArea;
         }
     }
